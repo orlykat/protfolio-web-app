eecd84f78a7a0bd2a359bed35283b48dfbbbfe68
orlykat <332269422@idf.il>
2024-08-05 19:22:12 +0300

    add first page large screen responsivity and a top bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px DimGrey',
+        DEFAULT: '0 2px 4px DimGrey',
+        lg: '0 0px 20px DimGrey',
+    },},
   },
-  plugins: [],
+  plugins: [ 
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),],
 }
-
